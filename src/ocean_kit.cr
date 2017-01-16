70599e307a5199846d8633a0560b3125d4f75081
osfx <osfx@yandex.com>
2017-01-16 11:32:24 +0300

    Add ssh_key integration

--- src/ocean_kit.cr
+++ src/ocean_kit.cr
@@ -1,5 +1,15 @@
-require "./ocean_kit/*"
-
+require "./ocean_kit/**"
+require "yaml"
+require "http/client"
+require "json"
 module OceanKit
-  # TODO Put your code here
+  extend self
+	def key
+    key = YAML.parse(File.read(".key"))
+    return key["API-KEY"].to_s
+  end
 end
+
+puts OceanKit.key
+ok = OceanKit::Client.new(OceanKit.key)
+puts ok.ssh_keys.find("3926194")
