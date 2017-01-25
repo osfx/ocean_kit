4f7586d84f582024bd3f3e7732944ccca4db6bcd
osfx <osfx@yandex.com>
2017-01-25 09:12:26 +0300

    Write readme

--- spec/ocean_kit_spec.cr
+++ spec/ocean_kit_spec.cr
@@ -1,11 +1,9 @@
 require "./spec_helper"
+
 describe OceanKit do
-	# subject { Greeting.new }
-	#
-	# describe "when name" do
-	# 	let(name) { "world" }
-	# 	it "greets" do
-	# 		expect(subject.greet(name)).to eq "Hello, world"
-	# 	end
-	# end
+	context "Client" do
+		it "work" do
+			expect(true).to eq true
+		end
+	end
 end
