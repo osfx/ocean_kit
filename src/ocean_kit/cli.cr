f24f81761ef7627b40b14d4a292bb58273575479
Peter Boriskin <osfx@yandex.com>
2017-01-25 09:12:26 +0300

    Write readme

---
+++ src/ocean_kit/cli.cr
@@ -0,0 +1,18 @@
+# require "./api"
+# require "./ocean_kit/api"
+
+module OceanKit
+	class CLI
+		@cli : String
+
+		def initialize
+			@cli = "dsadasdas"
+		end
+
+		def cli : String
+			@cli
+		end
+	end
+
+
+end
