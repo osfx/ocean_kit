32f0baaf0c5604b43b0784a4f07802d8fd002ae9
Peter Boriskin <osfx@yandex.com>
2017-02-15 17:54:35 +0400

    Update ocean_kit.cr

--- src/ocean_kit.cr
+++ src/ocean_kit.cr
@@ -9,16 +9,6 @@ require "yaml"
 
 
 module OceanKit
-
-
-	extend self
-	def key
-    key = YAML.parse(File.read(".key"))
-    return key["API-KEY"].to_s
-  end
-
+  extend self
 end
 
-
-
-# ok = OceanKit::Client.new(OceanKit.key)
