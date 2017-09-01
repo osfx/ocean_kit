4b629b6d2f493c0a628f7db108c61d2988da10dc
osfx <osfx@yandex.com>
2017-09-01 22:24:06 +0300

    Remove old specs

--- spec/ocean_kit_spec.cr
+++ spec/ocean_kit_spec.cr
@@ -1,9 +1,17 @@
 require "./spec_helper"
 
 describe OceanKit do
-	context "Client" do
-		it "work" do
-			expect(true).to eq true
-		end
-	end
+  context "Client" do
+    it "with invalid key no auth" do
+      client = OceanKit::Client.new("xxx")
+      (client.account.info).should eq "{\"id\": \"unauthorized\", \"message\": \"Unable to authenticate you.\" }"
+    end
+    it "with valid key no auth" do
+      client = OceanKit::Client.new(ENV["DIGITAL_OCEAN_API_KEY"])
+      (client.account.info).should match /uuid/
+    end
+    it "work" do
+      (true).should eq true
+    end
+  end
 end
--- spec/spec_helper.cr
+++ spec/spec_helper.cr
@@ -1,9 +1,2 @@
-# # require "webmock"
-require "../ocean_kit/client/connection"
-require "../ocean_kit/resources/base"
-require "../ocean_kit/resources/account"
-require "../ocean_kit/client"
 require "../ocean_kit"
-require "spec2"
-include Spec2::GlobalDSL
-Spec2.doc
+require "spec"
